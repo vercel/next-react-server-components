e528e3277914d7a38a19ffaa9d39c532b9749322
Lee Robinson <lrobinson2011@gmail.com>
2022-11-05 21:09:51 -0500

    Start moving to more app layout things

--- app/rsc/page.js
+++ app/rsc/page.js
@@ -1,53 +1,29 @@
-import { Suspense } from 'react';
-
-// Shared Components
-import Skeletons from '../../components/skeletons';
-
 // Server Components
 import SystemInfo from '../../components/server-info';
 
 // Client Components
-import Page from '../../components/page';
 import Story from '../../components/story';
 import Footer from '../../components/footer';
-import ErrorPlaceholder from '../../components/error-placeholder';
 
 // Utils
 import fetchData from '../../lib/fetch-data';
 import { transform } from '../../lib/get-item';
-import useData from '../../lib/use-data';
 
-function StoryWithData({ id }) {
-  const { data } = useData(`s-${id}`, () =>
-    fetchData(`item/${id}`).then(transform)
-  );
-  return <Story {...data} />;
+async function StoryWithData({ id }) {
+  const data = await fetchData(`item/${id}`);
+  return <Story {...transform(data)} />;
 }
 
-function NewsWithData() {
-  const { data: storyIds, error } = useData('top', () =>
-    fetchData('topstories')
-  );
+export default async function RSCPage() {
+  const storyIds = await fetchData('topstories');
+
   return (
     <>
-      {error ? <ErrorPlaceholder error={error} /> : null}
-      {storyIds
-        ? storyIds.slice(0, 30).map((id) => {
-            return <StoryWithData id={id} key={id} />;
-          })
-        : null}
-    </>
-  );
-}
-
-export default function RSCPage() {
-  return (
-    <Page>
-      <Suspense fallback={<Skeletons />}>
-        <NewsWithData />
-      </Suspense>
+      {storyIds.slice(0, 30).map((id) => {
+        return <StoryWithData id={id} key={id} />;
+      })}
       <Footer />
       <SystemInfo />
-    </Page>
+    </>
   );
 }
