f09e6add6de81be7617dd107d123d21705c13c3c
Lee Robinson <me@leerob.io>
2022-11-13 07:10:02 -1000

    Fix mobile styles and move to pnpm. (#54)

--- components/nav.js
+++ components/nav.js
@@ -2,12 +2,12 @@ import styles from './nav.module.css'
 
 export default function Nav() {
   return (
-    <ul className={styles["nav-ul"]}>
-      <Item href="/newest">new</Item>
-      <Item href="/show">show</Item>
-      <Item href="/ask">ask</Item>
-      <Item href="/jobs">jobs</Item>
-      <Item href="/submit">submit</Item>
+    <ul className={styles['nav-ul']}>
+      <Item href="/">new</Item>
+      <Item href="/">show</Item>
+      <Item href="/">ask</Item>
+      <Item href="/">jobs</Item>
+      <Item href="/">submit</Item>
     </ul>
   )
 }
