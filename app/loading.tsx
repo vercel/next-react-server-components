fae41057cea15240ac68fe18bb5462f0102e73b9
Lee Robinson <lrobinson2011@gmail.com>
2023-03-05 19:11:57 -0600

    Fix build

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,5 @@
-import Skeletons from '../components/skeletons';
+import Skeletons from 'components/skeletons'
 
 export default function Loading() {
-  return <Skeletons />;
+  return <Skeletons />
 }
--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
-    fetchCache: true
+    appDir: true
   },
   async rewrites() {
     return [
