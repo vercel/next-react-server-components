479b327f9d9941eba291100f87141daf6c9fc6ac
Jiachi Liu <inbox@huozhi.im>
2023-05-08 13:47:47 +0200

    Revert disabling banner prefetch and add index page

--- next.config.js
+++ next.config.js
@@ -4,14 +4,14 @@ const nextConfig = {
   experimental: {
     appDir: true
   },
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/news/1'
-      }
-    ]
-  }
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/news/1'
+  //     }
+  //   ]
+  // }
 }
 
 module.exports = nextConfig
