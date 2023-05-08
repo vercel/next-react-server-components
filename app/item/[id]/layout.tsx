dd8dc2ee01d7b57fa565493f0e9277c4b949a6ad
Sebastian Markbage <sebastian@calyptus.eu>
2023-05-07 20:07:22 -0400

    Use static optimizations
    Pregenerate the home page and the top stories.
    
    Keep these pages in ISR cache.

--- app/item/[id]/layout.tsx
+++ app/item/[id]/layout.tsx
@@ -3,6 +3,17 @@ import Story from 'components/story';
 import CommentForm from 'components/comment-form';
 import styles from './layout.module.css';
 import getItem from 'lib/get-item'
+import fetchData from 'lib/fetch-data'
+
+export const dynamicParams = true;
+
+export async function generateStaticParams() {
+  // Preload the top 30 stories
+  const storyIds = await fetchData('topstories')
+  return storyIds.slice(0, 30).map(id => ({
+    id: '' + id,
+  }));
+}
 
 export default async function ItemPage({ params, children }) {
   const { id } = params
--- app/news/[page]/page.tsx
+++ app/news/[page]/page.tsx
@@ -1,6 +1,12 @@
 import Stories from 'components/stories'
 import fetchData from 'lib/fetch-data'
 
+export const dynamicParams = true;
+
+export async function generateStaticParams() {
+  return [1];
+}
+
 export default async function RSCPage({ params }) {
   const { page } = params
   const storyIds = await fetchData('topstories')
