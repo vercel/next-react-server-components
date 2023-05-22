e48c8253d14ec3375b518dfe7bad88e3f30e9e51
Jiachi Liu <inbox@huozhi.im>
2023-05-23 00:09:48 +0200

    Bring rewrite back (#75)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: true,
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/news/1'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
